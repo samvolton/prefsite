b8ed93a41220c169de23ffcaa96fb07338c9c19a
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-02 01:26:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write("""  PREF SITE """)
 ticker = 'tsla'
 
 data = yf.Ticker(ticker)
-tickerOf = data.history(period= '1d', start='2022-05-01', end='2023-05-01'
+tickerDf = data.history(period= '1d', start='2022-05-01', end='2023-05-01'
 
-st.line_chart(tickerOf.Close)
-st.line_chart(tickerOf.Volume)                      
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)                      
